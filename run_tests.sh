91424a85c5606dc763123a392e0d5dc825c6c1d6
Harish Kumar <harish.kumar@mastercard.com>
2025-11-24 16:04:58 +0000

    test

--- run_tests.sh
+++ run_tests.sh
@@ -36,6 +36,92 @@ report_outcome() {
     fi
 }
 
+run() {
+    qemu_args=""
+    sdrun_args=""
+    bootindex=1
+
+    while [ $# -gt 0 ]
+    do
+        case "$1" in
+            --blockdev=*)
+                # Example: --blockdev installer:/path/to/installer.raw
+
+                arg="${1#--blockdev=}"
+                src="${arg#*:}"
+                id="${arg%%:*}"
+                qemu_args="${qemu_args}-blockdev driver=raw,node-name=${id},discard=unmap,file.driver=file,file.filename=${src},file.aio=io_uring,cache.direct=yes,cache.no-flush=yes -device virtio-blk-pci,drive=${id},serial=${id},bootindex=${bootindex} "
+                bootindex=$(($bootindex + 1))
+                shift
+                ;;
+            --wait|-P)
+                sdrun_args="${sdrun_args}$1 "
+                shift
+                ;;
+            -smbios)
+                qemu_args="${qemu_args}$1 $2 "
+                shift 2
+                ;;
+        esac
+    done
+
+    # Stop and reset previous swtpm if it exists (for 2nd and 3rd VM boots)
+    systemctl --user stop "mangos-test-${testid}-swtpm" 2>/dev/null || true
+    systemctl --user reset-failed "mangos-test-${testid}-swtpm" 2>/dev/null || true
+
+    $systemd_run -u "mangos-test-${testid}-swtpm" -q -d -- \
+        mkosi sandbox -- \
+        swtpm socket --tpmstate dir="${tpmdir}" --ctrl type=unixio,path="${tmpdir}/swtpm-sock" --tpm2
+
+    # Give swtpm a chance to start?
+    sleep 3
+
+    script="${tmpdir}/script.sh"
+    cat <<-EOF > "${script}"
+#!/bin/sh
+mkosi sandbox -- \
+        qemu-system-x86_64 \
+        -no-reboot \
+        -machine type=q35,smm=on,hpet=off \
+        -smp 2 \
+        -m 2048M \
+        -object rng-random,filename=/dev/urandom,id=rng0 \
+        -device virtio-rng-pci,rng=rng0,id=rng-device0 \
+        -device virtio-balloon,free-page-reporting=on \
+        -no-user-config \
+        -nic user,model=virtio-net-pci \
+        -cpu max \
+        -nographic \
+        -nodefaults \
+        -chardev stdio,mux=on,id=console,signal=off \
+        -device virtio-serial-pci,id=mkosi-virtio-serial-pci \
+        -device virtconsole,chardev=console \
+        -mon console \
+        -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.secboot.fd \
+        -drive if=pflash,format=raw,file='${tmpdir}/efivars.fd' \
+        -global ICH9-LPC.disable_s3=1 \
+        -global driver=cfi.pflash01,property=secure,value=on \
+        -device virtio-scsi-pci,id=mkosi \
+        ${qemu_args} \
+        -chardev socket,id=chrtpm,path='${tmpdir}/swtpm-sock'  \
+        -tpmdev emulator,id=tpm0,chardev=chrtpm \
+        -device tpm-tis,tpmdev=tpm0 \
+        -uuid '${uuid}' \
+        -smbios type=11,value=io.systemd.credential.binary:firstboot.locale=Qy5VVEYtOA== \
+        -smbios type=11,value=io.systemd.credential.binary:firstboot.timezone=QW1lcmljYS9Mb3NfQW5nZWxlcw== \
+        -smbios type=11,value=io.systemd.credential.binary:ssh.authorized_keys.root=$(ssh-keygen -y -f mkosi.key | base64 -w 0) \
+        -smbios type=11,value=io.systemd.credential.binary:vmm.notify_socket=$(echo -n vsock:2:23433 | base64 -w 0)  \
+        -smbios type=11,value=io.systemd.stub.kernel-cmdline-extra='systemd.wants=network.target module_blacklist=vmw_vmci systemd.tty.term.hvc0=xterm-256color systemd.tty.columns.hvc0=${cols} systemd.tty.rows.hvc0=${rows} ip=enc0:any ip=enp0s1:any ip=enp0s2:any ip=host0:any ip=none loglevel=4 SYSTEMD_SULOGIN_FORCE=1 systemd.tty.term.console=xterm-256color systemd.tty.columns.console=${cols} systemd.tty.rows.console=${rows} console=hvc0 TERM=xterm-256color' \
+        -smbios type=11,value=io.systemd.boot.kernel-cmdline-extra='systemd.wants=network.target module_blacklist=vmw_vmci systemd.tty.term.hvc0=xterm-256color systemd.tty.columns.hvc0=${cols} systemd.tty.rows.hvc0=${rows} ip=enc0:any ip=enp0s1:any ip=enp0s2:any ip=host0:any ip=none loglevel=4 SYSTEMD_SULOGIN_FORCE=1 systemd.tty.term.console=xterm-256color systemd.tty.columns.console=${cols} systemd.tty.rows.console=${rows} console=hvc0 TERM=xterm-256color' \
+        -device vhost-vsock-pci,guest-cid=42
+EOF
+    chmod +x "${script}"
+
+    $systemd_run -u "mangos-test-${testid}-qemu" -q -d -E TERM=xterm-256color ${sdrun_args} -- \
+        ${asciinema_rec} "${script}"
+    sleep 2
+}
+
 cols=120
 rows=40
 
@@ -129,89 +215,6 @@ target_disk="${tmpdir}/target_disk.raw"
 step Creating target disk
 $systemd_run -q -d --wait -- mkosi sandbox -- qemu-img create "${target_disk}" 20G
 report_outcome
-
-run() {
-    qemu_args=""
-    sdrun_args=""
-    bootindex=1
-
-    while [ $# -gt 0 ]
-    do
-        case "$1" in
-            --blockdev=*)
-                # Example: --blockdev installer:/path/to/installer.raw
-
-                arg="${1#--blockdev=}"
-                src="${arg#*:}"
-                id="${arg%%:*}"
-                qemu_args="${qemu_args}-blockdev driver=raw,node-name=${id},discard=unmap,file.driver=file,file.filename=${src},file.aio=io_uring,cache.direct=yes,cache.no-flush=yes -device virtio-blk-pci,drive=${id},serial=${id},bootindex=${bootindex} "
-                bootindex=$(($bootindex + 1))
-                shift
-                ;;
-            --wait|-P)
-                sdrun_args="${sdrun_args}$1 "
-                shift
-                ;;
-            -smbios)
-                qemu_args="${qemu_args}$1 $2 "
-                shift 2
-                ;;
-        esac
-    done
-
-    $systemd_run -u "mangos-test-${testid}-swtpm" -q -d -- \
-        mkosi sandbox -- \
-        swtpm socket --tpmstate dir="${tpmdir}" --ctrl type=unixio,path="${tmpdir}/swtpm-sock" --tpm2
-
-    # Give swtpm a chance to start?
-    sleep 3
-
-    script="${tmpdir}/script.sh"
-    cat <<-EOF > "${script}"
-#!/bin/sh
-mkosi sandbox -- \
-        qemu-system-x86_64 \
-        -no-reboot \
-        -machine type=q35,smm=on,hpet=off \
-        -smp 2 \
-        -m 4096M \
-        -object rng-random,filename=/dev/urandom,id=rng0 \
-        -device virtio-rng-pci,rng=rng0,id=rng-device0 \
-        -device virtio-balloon,free-page-reporting=on \
-        -no-user-config \
-        -nic user,model=virtio-net-pci \
-        -cpu host \
-        -accel kvm \
-        -nographic \
-        -nodefaults \
-        -chardev stdio,mux=on,id=console,signal=off \
-        -device virtio-serial-pci,id=mkosi-virtio-serial-pci \
-        -device virtconsole,chardev=console \
-        -mon console \
-        -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.secboot.fd \
-        -drive if=pflash,format=raw,file='${tmpdir}/efivars.fd' \
-        -global ICH9-LPC.disable_s3=1 \
-        -global driver=cfi.pflash01,property=secure,value=on \
-        -device virtio-scsi-pci,id=mkosi \
-        ${qemu_args} \
-        -chardev socket,id=chrtpm,path='${tmpdir}/swtpm-sock'  \
-        -tpmdev emulator,id=tpm0,chardev=chrtpm \
-        -device tpm-tis,tpmdev=tpm0 \
-        -uuid '${uuid}' \
-        -smbios type=11,value=io.systemd.credential.binary:firstboot.locale=Qy5VVEYtOA== \
-        -smbios type=11,value=io.systemd.credential.binary:firstboot.timezone=QW1lcmljYS9Mb3NfQW5nZWxlcw== \
-        -smbios type=11,value=io.systemd.credential.binary:ssh.authorized_keys.root=$(ssh-keygen -y -f mkosi.key | base64 -w 0) \
-        -smbios type=11,value=io.systemd.credential.binary:vmm.notify_socket=$(echo -n vsock:2:23433 | base64 -w 0)  \
-        -smbios type=11,value=io.systemd.stub.kernel-cmdline-extra='systemd.wants=network.target module_blacklist=vmw_vmci systemd.tty.term.hvc0=xterm-256color systemd.tty.columns.hvc0=${cols} systemd.tty.rows.hvc0=${rows} ip=enc0:any ip=enp0s1:any ip=enp0s2:any ip=host0:any ip=none loglevel=4 SYSTEMD_SULOGIN_FORCE=1 systemd.tty.term.console=xterm-256color systemd.tty.columns.console=${cols} systemd.tty.rows.console=${rows} console=hvc0 TERM=xterm-256color' \
-        -smbios type=11,value=io.systemd.boot.kernel-cmdline-extra='systemd.wants=network.target module_blacklist=vmw_vmci systemd.tty.term.hvc0=xterm-256color systemd.tty.columns.hvc0=${cols} systemd.tty.rows.hvc0=${rows} ip=enc0:any ip=enp0s1:any ip=enp0s2:any ip=host0:any ip=none loglevel=4 SYSTEMD_SULOGIN_FORCE=1 systemd.tty.term.console=xterm-256color systemd.tty.columns.console=${cols} systemd.tty.rows.console=${rows} console=hvc0 TERM=xterm-256color' \
-        -device vhost-vsock-pci,guest-cid=42
-EOF
-    chmod +x "${script}"
-
-    $systemd_run -u "mangos-test-${testid}-qemu" -q -d -E TERM=xterm-256color ${sdrun_args} -- \
-        ${asciinema_rec} "${script}"
-    sleep 2
-}
 
 IMAGE_VERSION="$(mkosi summary --json | jq .Images[0].ImageVersion -r)"
 installer="$(pwd)/out/mangos-installer_${IMAGE_VERSION}.raw"
